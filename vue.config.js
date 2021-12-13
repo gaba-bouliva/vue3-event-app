0f2839266c0705db30fedb8546d6dd0b69efd4e1
Gaba Bouliva <eaglebald111@gmail.com>
2021-12-13 03:56:49 +0100

    ajusted publicPath sub directory to github repo name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Real-World_Vue-3/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-event-app /' : '/'
 }
